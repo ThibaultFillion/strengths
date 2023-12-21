4bec5775c20c5c472a75db2e5e91485440272654
ThibaultFillion <145604448+ThibaultFillion@users.noreply.github.com>
2023-12-21 12:52:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
              sources = ["src/strengths/engines/strengths_engine/src/engine.cpp"],
              include_dirs = ["src/strengths/engines/strengths_engine/src"],
              define_macros = [("CPYEMVER", None)],
+             extra_compile_args = ["-std=c++11"],
              export_symbols = [
                 "Initialize3D",
                 "InitializeGraph",
