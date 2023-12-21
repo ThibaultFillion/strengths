aaeb7f98c0a1af0f00a3e47a0986f85fa468834c
ThibaultFillion <145604448+ThibaultFillion@users.noreply.github.com>
2023-12-21 10:45:22 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, Extension
+
+setup(
+     ext_modules = [
+         Extension(
+             name = "strengths.engines.strengths_engine.engine",
+             sources = ["src/strengths/engines/strengths_engine/src/engine.cpp"],
+             include_dirs = ["src/strengths/engines/strengths_engine/src"],
+             define_macros = [("CPYEMVER", None)],
+             export_symbols = [
+                "Initialize3D",
+                "InitializeGraph",
+                "Run",
+                "IterateN",
+                "Iterate",
+                "GetProgress",
+                "GetOutput",
+                "GetState",
+                "GetT",
+                "GetTSample",
+                "GetNSamples",
+                "Sample",
+                "Finalize"
+                ]
+             )
+         ]
+     )
