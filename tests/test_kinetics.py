f1b8d218950a34c1d8adfe405c3dce3010336313
ThibaultFillion <145604448+ThibaultFillion@users.noreply.github.com>
2024-05-21 01:06:32 +0200

    Update test_kinetics.py

--- tests/test_kinetics.py
+++ tests/test_kinetics.py
@@ -11,9 +11,9 @@ import numpy as np
 # test reaction rate
 # should be done both for rdnetworks and graphs
 
-def test_compute_reaction_rates_dimensions_for_different_reaction_orders_with_default_units_default_units(spacetype) :
+def _test_compute_reaction_rates_dimensions_for_different_reaction_orders_with_default_units_default_units(spacetype) :
     #checks that rates are in molecule/s, no mater the reaction order
- 
+
     system_dict = {
         "network" : {
             "species" : [
@@ -55,7 +55,7 @@ def test_compute_reaction_rates_dimensions_for_different_reaction_orders_with_de
         for rate in rates :
             assert rate.units.dim == UnitsDimensions(quantity=1, space=0, time=-1)
 
-def test_compute_reaction_rates_value(spacetype) :
+def _test_compute_reaction_rates_value(spacetype) :
     #checks that rates computed values are correct
     kf = 12
     kr = 0.5
@@ -67,9 +67,9 @@ def test_compute_reaction_rates_value(spacetype) :
     D = 548
     E = 0.33
     F = 1000
-    
+
     NA = avogadro_number()
-    state = UnitArray([A, B, C, D, E, F], "mol")    
+    state = UnitArray([A, B, C, D, E, F], "mol")
 
     system_dict = {
         "network" : {
@@ -94,13 +94,13 @@ def test_compute_reaction_rates_value(spacetype) :
             "edges" : []}
     else :
         system_dict["space"] = {"cell_vol" : str(V)+" mL"}
-        
+
     system = rdsystem_from_dict(system_dict)
 
     V *= 1e12
     rf = kf/60 * V * NA*A/V * (NA*C/V)**2 * (NA*B/V)**4 # molecule/s
     rr = kr * V * (NA*D/V)**6 * (NA*F/V)**3             # molecule/s
-    
+
     rates = strnkin.compute_reaction_rates(
         system = system,
         reaction = system.network.reactions[0],
@@ -111,10 +111,10 @@ def test_compute_reaction_rates_value(spacetype) :
     assert np.isclose(rates[0].convert("molecule/s").value, rf)
     assert np.isclose(rates[1].convert("molecule/s").value, rr)
 
-def test_compute_reaction_rates_with_environments(spacetype) :
+def _test_compute_reaction_rates_with_environments(spacetype) :
     #checks that reaction env are taken into account
 
-    state = UnitArray([1, 1, 1, 1], "molecule")        
+    state = UnitArray([1, 1, 1, 1], "molecule")
     system_dict = {
         "network" : {
             "environments" : ["a", "b"],
@@ -137,7 +137,7 @@ def test_compute_reaction_rates_with_environments(spacetype) :
         system_dict["space"] = {"w":2, "environments":[0, 1]}
 
     system = rdsystem_from_dict(system_dict)
-    
+
     rates = strnkin.compute_reaction_rates(
         system = system,
         reaction = system.network.reactions[0],
@@ -158,10 +158,10 @@ def test_compute_reaction_rates_with_environments(spacetype) :
     assert rates[0].convert("molecule/s").value == 1
     assert rates[1].convert("molecule/s").value == 1
 
-def test_compute_reaction_rates_in_inhomogeneous_state(spacetype) :
+def _test_compute_reaction_rates_in_inhomogeneous_state(spacetype) :
     #checks that reaction env are taken into account
 
-    state = UnitArray([1, 2, 3, 4], "molecule")        
+    state = UnitArray([1, 2, 3, 4], "molecule")
     system_dict = {
         "network" : {
             "species" : [
@@ -181,9 +181,9 @@ def test_compute_reaction_rates_in_inhomogeneous_state(spacetype) :
             "edges" : []}
     else :
         system_dict["space"] = {"w":2}
-        
+
     system = rdsystem_from_dict(system_dict)
-    
+
     rates = strnkin.compute_reaction_rates(
         system = system,
         reaction = system.network.reactions[0],
@@ -206,7 +206,7 @@ def test_compute_reaction_rates_in_inhomogeneous_state(spacetype) :
 
 #####################################
 
-def test_compute_diffusion_rates(spacetype) :
+def _test_compute_diffusion_rates(spacetype) :
 
     system_dict = {
         "network" : {
@@ -216,7 +216,7 @@ def test_compute_diffusion_rates(spacetype) :
             "reactions" : []
             }
         }
-    
+
     if spacetype == "graph" :
         system_dict["space"] = {
             "type" : "graph",
@@ -224,10 +224,10 @@ def test_compute_diffusion_rates(spacetype) :
             "edges" : [{"nodes" : [0, 1], "surface" : 1, "distance" : 1}]}
     else :
         system_dict["space"] = {"w" : 2}
-    
+
     system = rdsystem_from_dict(system_dict)
     #                  B  B  A  A  C  C
-    #                  0  1  0  1  0  1   
+    #                  0  1  0  1  0  1
     state = UnitArray([0, 0, 30, 40, 0, 0], "molecule")
 
     rates = strnkin.compute_diffusion_rates(
@@ -238,11 +238,11 @@ def test_compute_diffusion_rates(spacetype) :
         state = state,
         units_system = UnitsSystem()
         )
-    
+
     assert rates[0].value == 30
     assert rates[1].value == 40
 
-def test_compute_diffusion_rates_with_different_env(spacetype) :
+def _test_compute_diffusion_rates_with_different_env(spacetype) :
 
     system_dict = {
         "network" : {
@@ -251,7 +251,7 @@ def test_compute_diffusion_rates_with_different_env(spacetype) :
             "reactions" : []
             }
         }
-    
+
     if spacetype == "graph" :
         system_dict["space"] = {
             "type" : "graph",
@@ -259,7 +259,7 @@ def test_compute_diffusion_rates_with_different_env(spacetype) :
             "edges" : [{"nodes" : [0, 1], "surface" : 1, "distance" : 1}]}
     else :
         system_dict["space"] = {"w" : 2, "cell_env" : [0, 1]}
-    
+
     system = rdsystem_from_dict(system_dict)
     state = UnitArray([30, 40], "molecule")
 
@@ -275,7 +275,7 @@ def test_compute_diffusion_rates_with_different_env(spacetype) :
     assert rates[0].value == 0
     assert rates[1].value == 0
 
-def test_compute_diffusion_rates_with_different_units(spacetype) :
+def _test_compute_diffusion_rates_with_different_units(spacetype) :
 
     system_dict = {
         "network" : {
@@ -283,7 +283,7 @@ def test_compute_diffusion_rates_with_different_units(spacetype) :
             "reactions" : []
             }
         }
-    
+
     if spacetype == "graph" :
         system_dict["space"] = {
             "type" : "graph",
@@ -291,10 +291,10 @@ def test_compute_diffusion_rates_with_different_units(spacetype) :
             "edges" : [{"nodes" : [0, 1], "surface" : "1 nm2", "distance" : "1 nm"}]}
     else :
         system_dict["space"] = {"w" : 2, "cell_vol" : "1 nm3"}
-    
+
     system = rdsystem_from_dict(system_dict)
     #                  B  B  A  A  C  C
-    #                  0  1  0  1  0  1   
+    #                  0  1  0  1  0  1
     state = UnitArray([30, 40], "mol")
 
     rates = strnkin.compute_diffusion_rates(
@@ -311,8 +311,8 @@ def test_compute_diffusion_rates_with_different_units(spacetype) :
 
 #####################################
 
-def test_compute_dspeciesdt(spacetype) :
-    
+def _test_compute_dspeciesdt(spacetype) :
+
     system_dict = {
         "network" : {
             "species" : [
@@ -325,7 +325,7 @@ def test_compute_dspeciesdt(spacetype) :
             }
         }
 
-    if spacetype == "graph" : 
+    if spacetype == "graph" :
         system_dict["space"] = {
             "type" : "graph",
             "nodes" : [{}, {}],
@@ -340,7 +340,7 @@ def test_compute_dspeciesdt(spacetype) :
     #                  A   A   B   B
     #                  0   1   0   1
     state = UnitArray([10, 20, 1,  2], "molecule")
-    
+
     dA = strnkin.compute_dspeciesdt(
         system = system,
         species = "A",
@@ -352,8 +352,10 @@ def test_compute_dspeciesdt(spacetype) :
     assert dA.value == (-10*1 + 1*1   -   10*1 + 20*1)
     assert dA.units.dim == UnitsDimensions(space=0, time=-1, quantity=1)
 
+#####################################
+
 def test_compute_dspeciesdt_with_boundary_conditions() :
-    
+
     system_dict = {
         "network" : {
             "species" : [
@@ -374,7 +376,7 @@ def test_compute_dspeciesdt_with_boundary_conditions() :
     #                  A   A   B   B
     #                  0   1   0   1
     state = UnitArray([10, 20, 1,  2], "molecule")
-    
+
     dA = strnkin.compute_dspeciesdt(
         system = system,
         species = "A",
@@ -384,9 +386,9 @@ def test_compute_dspeciesdt_with_boundary_conditions() :
         )
 
     assert dA.value == (-10*1 + 1*1   +   2*(-10*1 + 20*1))
-    
+
     #############"
-    
+
     system_dict = {
         "network" : {
             "species" : [
@@ -407,7 +409,7 @@ def test_compute_dspeciesdt_with_boundary_conditions() :
     #                  A   A   B   B
     #                  0   1   0   1
     state = UnitArray([10, 20, 1,  2], "molecule")
-    
+
     dA = strnkin.compute_dspeciesdt(
         system = system,
         species = "A",
@@ -418,30 +420,58 @@ def test_compute_dspeciesdt_with_boundary_conditions() :
 
     assert dA.value == (-10*1 + 1*1   +   (-10*1 + 20*1))
 
-def run_all_tests() :
-  test_compute_reaction_rates_dimensions_for_different_reaction_orders_with_default_units_default_units("grid")
-  test_compute_reaction_rates_value("grid")
-  test_compute_reaction_rates_with_environments("grid")
-  test_compute_reaction_rates_in_inhomogeneous_state("grid")
-  
-  test_compute_reaction_rates_dimensions_for_different_reaction_orders_with_default_units_default_units("graph")
-  test_compute_reaction_rates_value("graph")
-  test_compute_reaction_rates_with_environments("graph")
-  test_compute_reaction_rates_in_inhomogeneous_state("graph")
-  
-  ############
-  
-  test_compute_diffusion_rates("grid")
-  test_compute_diffusion_rates_with_different_env("grid")
-  test_compute_diffusion_rates_with_different_units("grid")
-  
-  test_compute_diffusion_rates("graph")
-  test_compute_diffusion_rates_with_different_env("graph")
-  test_compute_diffusion_rates_with_different_units("graph")
-  
-  ############
-  
-  test_compute_dspeciesdt("grid")
-  test_compute_dspeciesdt_with_boundary_conditions()
-  
-  test_compute_dspeciesdt("graph")
+#####################################
+# graph versions
+
+def test_compute_reaction_rates_dimensions_for_different_reaction_orders_with_default_units_default_units__graph():
+    _test_compute_reaction_rates_dimensions_for_different_reaction_orders_with_default_units_default_units("graph")
+
+def test_compute_reaction_rates_value__graph():
+    _test_compute_reaction_rates_value("graph")
+
+def test_compute_reaction_rates_with_environments__graph():
+    _test_compute_reaction_rates_with_environments("graph")
+
+def test_compute_reaction_rates_in_inhomogeneous_state__graph():
+    _test_compute_reaction_rates_in_inhomogeneous_state("graph")
+
+def test_compute_diffusion_rates__graph():
+    _test_compute_diffusion_rates("graph")
+
+def test_compute_diffusion_rates_with_different_env__graph():
+    _test_compute_diffusion_rates("graph")
+
+def test_compute_diffusion_rates_with_different_units__graph():
+    _test_compute_diffusion_rates_with_different_units("graph")
+
+def test_compute_dspeciesdt__graph():
+    _test_compute_dspeciesdt("graph")
+
+#####################################
+# grid versions
+
+def test_compute_reaction_rates_dimensions_for_different_reaction_orders_with_default_units_default_units__grid():
+    _test_compute_reaction_rates_dimensions_for_different_reaction_orders_with_default_units_default_units("grid")
+
+def test_compute_reaction_rates_value__grid():
+    _test_compute_reaction_rates_value("grid")
+
+def test_compute_reaction_rates_with_environments__grid():
+    _test_compute_reaction_rates_with_environments("grid")
+
+def test_compute_reaction_rates_in_inhomogeneous_state__grid():
+    _test_compute_reaction_rates_in_inhomogeneous_state("grid")
+
+def test_compute_diffusion_rates__grid():
+    _test_compute_diffusion_rates("grid")
+
+def test_compute_diffusion_rates_with_different_env__grid():
+    _test_compute_diffusion_rates("grid")
+
+def test_compute_diffusion_rates_with_different_units__grid():
+    _test_compute_diffusion_rates_with_different_units("grid")
+
+def test_compute_dspeciesdt__grid():
+    _test_compute_dspeciesdt("grid")
+
+#####################################
